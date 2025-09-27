e990c5310bf2bf799d963962334bdda47bded021
ibrahimkhan125 <m.ibrahim8882@gmail.com>
2025-09-27 13:04:28 +0100

    making seeders and factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->sentence;
+        return [
+            'image' => $this->faker->imageUrl(),
+            'title' => $title,
+            'slug' => Str::slug($title),
+            'context' => $this->faker->paragraph(5),
+            'category_id' => Category::inRandomOrder()->first(),
+            'user_id' => 1,
+            'published_at' => fake()->optional()->dateTime() 
+        ];
+    }
+}
